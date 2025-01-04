35a545f44123e371cd3531b4b692083bb3899e6d
ankitanjana23 <ankitanjana23@gamil.com>
2025-01-04 11:38:41 +0530

    Update project: Final

--- Frontend/src/pages/ProtectedPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { withAuthProtection } from '../utils/auth';
-
-const ProtectedPage = () => {
-  return <h1>Welcome to the protected page!</h1>;
-};
-
-export default withAuthProtection(ProtectedPage);
